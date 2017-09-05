9b1156d226c3a6c973caf201e1656039a5c66764
Kenny Gamer <KGamer@gamerpackaging.com>
2017-09-04 22:22:05 -0400

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Charlie Gamer on 9/4/17.
+//  Copyright © 2017 Charlie Gamer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
